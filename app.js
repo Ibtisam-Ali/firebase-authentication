3ac212531d30e94b99b5644c4fbe1115628f730f
Ibtisam-Ali <alimemonpk17@gmail.com>
2024-07-31 03:33:25 +0500

    fire-store

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-import {  signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import {  signInWithEmailAndPassword,
+   GoogleAuthProvider ,
+   signInWithPopup,
+   GithubAuthProvider  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 import { auth } from "./config.js";
-
 
 
 const form = document.querySelector('#form')
 const email = document.querySelector('#email')
 const password = document.querySelector('#pass')
+const google = document.querySelector('#google')
+const github = document.querySelector('#github')
 
 
-form.addEventListener('submit' , (event)=>{
+form.addEventListener('submit' ,async (event)=>{
     event.preventDefault();
 
     signInWithEmailAndPassword(auth, email.value, password.value)
@@ -16,12 +20,60 @@ form.addEventListener('submit' , (event)=>{
       const user = userCredential.user;
       console.log(user);
       window.location = 'home.html'
+
     })
     .catch((error) => {
-      const errorCode = error.code;
       const errorMessage = error.message;
       console.log(errorMessage);
     });
 
 
 })
+
+const provider = new GoogleAuthProvider();
+
+google.addEventListener('click' , ()=>{
+  console.log('google');
+
+  signInWithPopup(auth , provider)
+  .then((result) => {
+  
+    var user = result.user;
+   console.log(user);
+
+   window.location = 'home.html'
+  }).catch((error) => {
+    
+    var errorMessage = error.message;
+    console.log(errorMessage);
+  });
+});
+
+
+
+const githubProvider = new GithubAuthProvider();
+
+github.addEventListener('click' , ()=>{
+  console.log('');
+
+
+  signInWithPopup(auth, githubProvider)
+  .then((result) => {
+   
+    
+    var user = result.user;
+   console.log(User);
+
+
+
+  }).catch((error) => {
+    
+    var errorMessage = error.message;
+   console.log(errorMessage
+    
+   );
+  });
+
+});
+
+
--- home.js
+++ home.js
@@ -1,10 +1,16 @@
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-import { auth } from "./config.js";
+import { collection, addDoc, getDocs  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"; 
+import {auth, db } from "./config.js";
+
+
+ 
+
 
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
       const uid = user.uid;
+      // console.log(user);
       
     } else {
       window.location = 'index.html'
@@ -18,9 +24,59 @@ onAuthStateChanged(auth, (user) => {
         console.log('logout successfully');
         window.location = 'index.html'
       }).catch((error) => {
-        console.log(error
-            
-        );
+        console.log(error);
       });
       
   })
+
+
+  async function render() {
+
+    const arr =[];
+
+    const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    arr.push(doc.data())
+    
+  });
+  console.log(arr);
+
+};
+  
+render()
+  
+  
+  
+  
+    
+  
+  
+
+
+
+const form = document.querySelector('#form')
+const email = document.querySelector('#email')
+const password = document.querySelector('#pass')
+
+
+form.addEventListener('submit' , async (event)=>{
+    event.preventDefault();
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
+          email:email.value,
+          password:password.value,
+    
+
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+      
+    console.log(email.value);
+    console.log(password.value);
+    
+});
+
+
+ 
